88dc1e02a63b22e38b74ddc9ede57308825f9815
theharshittiwari <145567459+theharshittiwari@users.noreply.github.com>
2023-10-11 23:45:31 +0530

    advance stair problem.c

---
+++ advance stair problem.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+int stairs(int n){
+if (n==1||n==2)
+return n;
+if(n==3)
+
+return 4;
+else
+return stairs(n-1)+stairs(n-2)+stairs(n-3);
+}
+int main()
+{ int n,ways;
+printf("enter the value of n");
+scanf("%d",&n);
+ways=stairs(n);
+printf("%d",ways);
+
+    return 0;
+}
